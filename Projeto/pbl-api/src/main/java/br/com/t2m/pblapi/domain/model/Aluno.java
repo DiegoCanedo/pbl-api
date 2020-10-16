46721b30d11364cb9d8d85f5621e97fecc6f804c
Diego <diego.canedo86@gmail.com>
2020-10-16 13:21:52 -0300

    Aluno getById e Update

--- Projeto/pbl-api/src/main/java/br/com/t2m/pblapi/domain/model/Aluno.java
+++ Projeto/pbl-api/src/main/java/br/com/t2m/pblapi/domain/model/Aluno.java
@@ -1,13 +1,6 @@
 package br.com.t2m.pblapi.domain.model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -21,9 +14,7 @@ public class Aluno extends Usuario{
 	private String matricula;
 	@NotNull
 	private String nome;
-	@NotNull
-	private String periodo;
-
+	
 	public String getMatricula() {
 		return matricula;
 	}
@@ -38,14 +29,6 @@ public class Aluno extends Usuario{
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public String getPeriodo() {
-		return periodo;
-	}
-
-	public void setPeriodo(String periodo) {
-		this.periodo = periodo;
 	}
 	
 }
