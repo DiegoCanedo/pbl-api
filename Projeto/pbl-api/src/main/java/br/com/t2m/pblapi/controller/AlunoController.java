e307df6ed310210137d7327f77a018161b9a6d94
Diego <diego.canedo86@gmail.com>
2020-10-17 20:28:14 -0300

    Deletar Aluno

--- Projeto/pbl-api/src/main/java/br/com/t2m/pblapi/controller/AlunoController.java
+++ Projeto/pbl-api/src/main/java/br/com/t2m/pblapi/controller/AlunoController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,5 +41,11 @@ public class AlunoController {
 	@PutMapping("/{id}")
 	public ResponseEntity<AlunoDTO> alterar(@Valid @RequestBody AlunoDTO aluno){
 		return ResponseEntity.ok().body(alunoService.update(aluno));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deletar(@PathVariable Long id){
+		alunoService.delete(id);
+		return ResponseEntity.ok().body("Usuario " + id.toString() + " excluido com sucesso.");
 	}
 }
