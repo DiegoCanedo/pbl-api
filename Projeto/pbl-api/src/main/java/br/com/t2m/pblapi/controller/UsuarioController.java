a205082e0dd721d6544c6bdb6a65a0c61fb20533
Vhugo2001 <vitorhugolb2001@gmail.com>
2020-10-23 18:07:50 -0300

    CrossOrigin e RequestMapping adicionados no usuario

--- Projeto/pbl-api/src/main/java/br/com/t2m/pblapi/controller/UsuarioController.java
+++ Projeto/pbl-api/src/main/java/br/com/t2m/pblapi/controller/UsuarioController.java
@@ -5,10 +5,12 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +19,8 @@ import br.com.t2m.pblapi.domain.service.AlunoService;
 import br.com.t2m.pblapi.domain.service.dto.AlunoDTO;
 import br.com.t2m.pblapi.domain.service.dto.UsuarioIsAtivoDTO;
 import br.com.t2m.pblapi.domain.service.dto.UsuarioIsExcluidoDTO;
-
+@RequestMapping("/usuario")
+@CrossOrigin
 @RestController
 public class UsuarioController {
 
