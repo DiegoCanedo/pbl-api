b94b04a5b24ad052f35b36a957d46cfab250ffdd
Ronaldo Rosário Ramos <ronaldo-ramos@hotmail.com>
2020-11-05 19:40:34 -0300

    evoluindo backend de empresa

--- Projeto/pbl-api/src/main/java/br/com/t2m/pblapi/domain/model/Empresa.java
+++ Projeto/pbl-api/src/main/java/br/com/t2m/pblapi/domain/model/Empresa.java
@@ -1,31 +1,64 @@
 package br.com.t2m.pblapi.domain.model;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
-//@Entity
-//@Table(name = "empresa")
-public class Empresa {
-	
-	@Id
-	@NotNull
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id_empresa")
-	private Long id;
-	
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "empresa")
+@PrimaryKeyJoinColumn(name = "id_usuario")
+public class Empresa extends Usuario implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3942248280086506347L;
+
 	@NotBlank
 	private String nome;
-	
+
 	@NotBlank
 	private String cnpj;
-	
-	@NotBlank
-	private String email;
-	
-	
-	
+
+	private String endereco;
+
+	private String urlLog;
+
+	// private String focalPoint;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getUrlLog() {
+		return urlLog;
+	}
+
+	public void setUrlLog(String urlLog) {
+		this.urlLog = urlLog;
+	}
+
 }
---
+++ Projeto/pbl-api/src/main/java/br/com/t2m/pblapi/domain/service/EmpresaService.java
@@ -0,0 +1,146 @@
+package br.com.t2m.pblapi.domain.service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.t2m.pblapi.config.Constants;
+import br.com.t2m.pblapi.domain.model.EPerfil;
+import br.com.t2m.pblapi.domain.model.Empresa;
+import br.com.t2m.pblapi.domain.model.Perfil;
+import br.com.t2m.pblapi.domain.repository.IEmpresaRepository;
+import br.com.t2m.pblapi.domain.repository.IPerfilRepository;
+import br.com.t2m.pblapi.domain.service.dto.EmpresaDTO;
+import br.com.t2m.pblapi.domain.service.dto.UsuarioIsAtivoDTO;
+import br.com.t2m.pblapi.domain.service.dto.UsuarioIsExcluidoDTO;
+import br.com.t2m.pblapi.domain.service.mapper.EmpresaMapper;
+import br.com.t2m.pblapi.exception.ResourceAlreadyExistsException;
+import br.com.t2m.pblapi.exception.ResourceNotFoundException;
+
+
+
+@Service
+@Transactional
+public class EmpresaService {
+	
+	@Autowired
+	IEmpresaRepository empresaRepository;
+	
+	@Autowired
+	EmpresaMapper empresaMapper;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	IPerfilRepository perfilRepository;
+
+	@Transactional(readOnly = true)
+	public List<EmpresaDTO> getAll() {
+		return empresaRepository.findAll().stream().map(EmpresaDTO::new).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public EmpresaDTO getById(Long id) {
+		Optional<EmpresaDTO> opt = empresaRepository.findById(id).map(EmpresaDTO::new);
+
+		if (opt.isEmpty())
+			throw new ResourceNotFoundException(Constants.EMPRESA_NÃO_ENCONTRADA, id.toString());
+
+		return opt.get();
+	}
+
+	public Empresa insert(EmpresaDTO empresaDTO, String senha) {
+		
+		Optional<Perfil> optPerfil = perfilRepository.findByNome(EPerfil.ROLE_EMPRESA);
+		
+		if(optPerfil.isEmpty())
+		{
+			throw new ResourceNotFoundException(Constants.PERFIL_NAO_ENCONTRADO, EPerfil.ROLE_EMPRESA.name());
+		}
+
+		if (empresaDTO.getId() != null)
+
+			if (empresaRepository.existsByCnpj(empresaDTO.getCnpj()))
+				throw new ResourceAlreadyExistsException(Constants.USUARIO_CNPJ_JA_EXISTE, empresaDTO.getCnpj());
+
+		
+		Empresa empresa = empresaMapper.EmpresaDTOTOEmpresa(empresaDTO);
+		
+		empresa.setSenha(passwordEncoder.encode(senha));
+		Set<Perfil> perfis = new HashSet<>();
+
+		perfis.add(optPerfil.get());
+		empresa.setPerfil(perfis);
+		
+		empresa.setAtivo(false);
+		empresa.setExcluido(false);
+		return empresaRepository.save(empresa);
+
+	}
+
+	public EmpresaDTO update(EmpresaDTO empresaDTO) {
+		Optional<Empresa> opt = empresaRepository.findById(empresaDTO.getId());
+
+		opt.ifPresentOrElse(o -> this.validaUsuario(o, empresaDTO), () -> {
+			throw new ResourceNotFoundException(Constants.EMPRESA_NÃO_ENCONTRADA, empresaDTO.getId().toString());
+		});
+
+		// TODO Falta setar o array de perfil;
+		return Optional.of(opt).filter(Optional::isPresent).map(Optional::get).map(empresa -> {
+			empresa.setNome(empresaDTO.getNome());
+			empresa.setCnpj(empresaDTO.getCnpj());
+			empresa.setAtivo(empresaDTO.isAtivo());
+			return empresa;
+		}).map(EmpresaDTO::new).get();
+	}
+	
+	public EmpresaDTO updateAtivo(UsuarioIsAtivoDTO empresaIsAtivoDTO, Long id) {
+		Optional<Empresa> opt = empresaRepository.findById(id);
+
+		// TODO Falta setar o array de perfil;
+		return Optional.of(opt).filter(Optional::isPresent).map(Optional::get).map(empresa -> {
+			empresa.setId(id);
+			empresa.setAtivo(empresaIsAtivoDTO.isAtivo());
+			return empresa;
+		}).map(EmpresaDTO::new).get();
+	}
+	
+	public EmpresaDTO updateExcluido(UsuarioIsExcluidoDTO empresaIsExcluidoDTO, Long id) {
+		Optional<Empresa> opt = empresaRepository.findById(id);
+
+		// TODO Falta setar o array de perfil;
+		return Optional.of(opt).filter(Optional::isPresent).map(Optional::get).map(empresa -> {
+			empresa.setId(id);
+			empresa.setExcluido(empresaIsExcluidoDTO.isExcluido());
+			return empresa;
+		}).map(EmpresaDTO::new).get();
+	}
+
+	public void delete(Long id) {
+		Optional<Empresa> opt = empresaRepository.findById(id);
+
+		if (opt.isEmpty())
+			throw new ResourceNotFoundException(Constants.EMPRESA_NÃO_ENCONTRADA, id.toString());
+
+		Empresa empresa = opt.get();
+		empresa.setExcluido(true);
+		empresa.setAtivo(false);
+		empresaRepository.save(empresa);
+	}
+
+	private void validaUsuario(Empresa antigo, EmpresaDTO novo) {
+		if (!novo.getCnpj().equalsIgnoreCase(antigo.getCnpj()) && empresaRepository.existsByCnpj(novo.getCnpj())) {
+			throw new ResourceAlreadyExistsException(Constants.USUARIO_CNPJ_JA_EXISTE, novo.getCnpj());
+		}
+	}
+
+
+}
