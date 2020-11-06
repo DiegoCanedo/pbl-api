a53f3da1ca69ea01fec726fb98583ef5f747b261
Abner Joia <abnerjoia1991@gmail.com>
2020-11-06 16:36:36 -0300

    Tarefa DTO PostTarefaDTO

---
+++ Projeto/pbl-api/src/main/java/br/com/t2m/pblapi/domain/service/dto/TarefaDTO.java
@@ -0,0 +1,76 @@
+package br.com.t2m.pblapi.domain.service.dto;
+
+import java.time.Instant;
+import java.util.Date;
+
+import br.com.t2m.pblapi.domain.model.Tarefa;
+
+public class TarefaDTO {
+	
+	private Long id;
+	private Instant dataCriacao;
+	private String descricao;
+	private Date dataConclusao;
+	private boolean concluido;
+	
+	
+	public TarefaDTO(Tarefa entidade) {
+		super();
+		this.id = entidade.getId();
+		this.dataCriacao = entidade.getDataCriacao();
+		this.descricao = entidade.getDescricao();
+		this.dataConclusao = entidade.getDataConclusao();
+		this.concluido = entidade.isConcluido();
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public Instant getDataCriacao() {
+		return dataCriacao;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public Date getDataConclusao() {
+		return dataConclusao;
+	}
+
+
+	public boolean isConcluido() {
+		return concluido;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public void setDataCriacao(Instant dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public void setDataConclusao(Date dataConclusao) {
+		this.dataConclusao = dataConclusao;
+	}
+
+
+	public void setConcluido(boolean concluido) {
+		this.concluido = concluido;
+	}	
+
+}
