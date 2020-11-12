60de9736662c72a1c7270a0743c2213bc2aedfe6
Diego <diego.canedo86@gmail.com>
2020-11-12 15:53:27 -0300

    incluindo request mapping de usuario

--- Projeto/pbl-api/src/main/java/br/com/t2m/pblapi/controller/UsuarioController.java
+++ Projeto/pbl-api/src/main/java/br/com/t2m/pblapi/controller/UsuarioController.java
@@ -4,9 +4,12 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.t2m.pblapi.domain.service.AlunoService;
@@ -16,7 +19,10 @@ import br.com.t2m.pblapi.domain.service.dto.UsuarioIsExcluidoDTO;
 import io.swagger.annotations.Api;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/usuario")
 @Api(description = "rest api para usuario", tags = { "Usuario" })
+@PreAuthorize("hasAnyRole('ROLE_PROFESSOR')")
 public class UsuarioController {
 
 	@Autowired
